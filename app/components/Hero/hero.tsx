e9e0b266cbdc74b001492c8bf905d3683fabfb91
Moha Muhsin <mohamuhsin101@gmail.com>
2025-09-11 22:35:57 +0300

    rectified hero

--- app/components/Hero/hero.tsx
+++ app/components/Hero/hero.tsx
@@ -5,14 +5,14 @@ import HeroContent from "./hero-content";
 
 export default function Hero() {
   return (
-    <section className="relative w-full min-h-[80vh] md:min-h-[90vh] lg:min-h-screen overflow-hidden">
+    <section className="relative w-full min-h-[80vh] md:min-h-[90vh] lg:min-h-screen overflow-hidden flex items-center justify-center">
       {/* Background slider */}
-      <div className="absolute inset-0 top-0">
+      <div className="absolute inset-0 w-full h-full">
         <HeroSlider />
       </div>
 
-      {/* Overlay content, perfectly centered */}
-      <div className="absolute inset-0 flex items-center justify-center">
+      {/* Overlay content */}
+      <div className="relative z-10 w-full px-4 sm:px-6 lg:px-8 max-w-6xl">
         <HeroContent />
       </div>
     </section>
